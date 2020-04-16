5e44649b49a9a0f50830a337f469dcebb7eab726
unknown <1643171221@qq.com>
2020-04-16 13:02:35 +0800

    test add and commit

---
+++ test.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class traveler{
+   private:
+       string name;
+   public:
+       traveler(string name)
+       {
+        this->name=name;
+       }
+       string getName()
+       {
+         return name;
+       }
+};
+class hotel{
+  private:
+      traveler a;
+      int price;
+  public:
+      hotel(traveler a,int price)
+      {
+    this->a=a;
+    this->price=price;
+    }
+    void show()
+    {
+         cout<<"welcome!"<<endl<<"your name is:"<<static_cast<string>(a.getName())<<endl<<"you need to pay:"<<price<<endl;
+    }
+};
+
+int main()
+{
+    string name="lql";
+    traveler a(name);
+    hotel t(a,325);
+    t.show();
+}
